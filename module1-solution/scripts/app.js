87b8b2e516fb3161fecf4c4d8b88a33309be7d92
bestc0der <hrammohan@yahoo.com>
2016-09-17 09:19:48 -0400

    delta

--- module1-solution/scripts/app.js
+++ module1-solution/scripts/app.js
@@ -3,10 +3,45 @@
     var app = angular.module('LunchCheck',[]);
     app.controller("LunchCheckController", lunchCheckCtrl);
     function lunchCheckCtrl($scope){
-        $scope.name = "Ram";
-        $scope.sayHello = function()
+        $scope.lunchItems = "";
+        $scope.showMsg = false;
+        $scope.msgErrorClass = "label label-danger";
+        $scope.msgSuccessClass = "label label-success";
+        $scope.msg="";
+        $scope.msgClass = $scope.msgSuccessClass;
+        $scope.msgs.enterData = "Please enter data first";
+        $scope.msgs.manyItems = "Too much!";
+        $scope.msgs.success = "Enjoy!";
+        $scope.itemsChanged = function()
         {
-            return "Hello "
+            $scope.showMsg = false;
+            
+        };
+        
+        $scope.checkIfTooMuch = function()
+        {
+            if($scope.lunchItems.length === 0)
+            {
+                $scope.showMsg = true;
+                $scope.msgClass = $scope.msgErrorClass;
+                $scope.msg=$scope.msgs.enterData;
+                return;
+            }
+            
+            if($scope.lunchItems.split(",").length>3)
+            {
+                $scope.showMsg = true;
+                $scope.msgClass = $scope.msgErrorClass;
+                $scope.msg=$scope.msgs.manyItems;
+                return;
+            }
+            else{
+                $scope.showMsg = true;
+                $scope.msgClass = $scope.msgErrorClass;
+                $scope.msg=$scope.msgs.success;
+                return;                
+            }
+            
         }
     }
 })();
