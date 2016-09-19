54b76281c0c17819d4a0056a072851c49f564555
bestc0der <hrammohan@yahoo.com>
2016-09-18 21:12:46 -0400

    delta

--- module2-solution/scripts/app.js
+++ module2-solution/scripts/app.js
@@ -1,21 +1,24 @@
 (function(){
     'use strict';
     var app = angular.module('MyShoppingListApp',[]);
-    app.controller("ToBuyShoppingController ", ToBuyShoppingCtrl)
+    app.controller("ToBuyShoppingController ", ToBuyShoppingCtrl);
     app.controller("AlreadyBoughtShoppingController ", AlreadyBoughtShoppingCtrl);
     app.service("ShoppingListCheckOffService", ShoppingListCheckOffSvc);
     
-    
     function ShoppingListCheckOffSvc(){
-        this.ToBuy = [{ name: "Cookies", quantity: 10 },{ name: "Biscuit", quantity: 11 },
-        { name: "Soda", quantity: 12 }, { name: "Chips", quantity: 13 }, { name: "Mint", quantity: 14 }];
+        var svc = this;
+        svc.ToBuy = [{ name: "Cookies", quantity: 10 },
+        { name: "Biscuit", quantity: 11 },
+        { name: "Soda", quantity: 12 }, 
+        { name: "Chips", quantity: 13 }, 
+        { name: "Mint", quantity: 14 }];
         
-        this.AlreadyBought = [];
+        svc.AlreadyBought = [];
         
-        this.BoughtItem = function(item)
+        svc.BoughtItem = function(item)
         {
-            this.ToBuy = this.ToBuy.filter( function(item1){ return item1 != item});
-            this.AlreadyBought.push(item);
+            svc.ToBuy = svc.ToBuy.filter( function(item1){ return item1 != item});
+            svc.AlreadyBought.push(item);
         }
         
     };
