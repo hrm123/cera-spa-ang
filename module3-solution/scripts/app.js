d6af2f31e6dd2caf46dad05b2368d2f8cb2e788d
bestc0der <hrammohan@yahoo.com>
2016-09-29 04:37:08 -0400

    delta

--- module3-solution/scripts/app.js
+++ module3-solution/scripts/app.js
@@ -3,7 +3,7 @@
     var app = angular.module('NarrowItDownApp',[]);
     app.controller('NarrowItDownController', narrowItDownController);
     app.factory('MenuSearchService', menuSearchSvc);
-    app.directive("FoundItems", foundItemsWidget)
+    app.directive("FoundItems", foundItemsWidget);
     
     function foundItemsWidget(){
         var ddo = {
