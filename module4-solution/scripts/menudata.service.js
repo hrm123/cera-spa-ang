62eaac03cbe9baed56585068eade6acded1a1a92
bestc0der <hrammohan@yahoo.com>
2016-11-15 16:44:26 -0500

    delta

--- module4-solution/scripts/menudata.service.js
+++ module4-solution/scripts/menudata.service.js
@@ -13,9 +13,9 @@
         {
             return $http.get('https://davids-restaurant.herokuapp.com/categories.json').then(function (result) {
                 // process result and only keep items that match
-                var foundItems = result.data.menu_items.filter( function( item ) { return item.description.indexOf(searchTerm) >0; } )
+                //var foundItems = result.data.menu_items.filter( function( item ) { return item.description.indexOf(searchTerm) >0; } )
                 // return processed items
-                return foundItems;
+                return result;
             });
         }
 
@@ -23,9 +23,9 @@
         {
             return $http.get('https://davids-restaurant.herokuapp.com/menu_items.json?category='+categoryShortName).then(function (result) {
                 // process result and only keep items that match
-                var foundItems = result.data.menu_items.filter( function( item ) { return item.description.indexOf(searchTerm) >0; } )
+                //var foundItems = result.data.menu_items.filter( function( item ) { return item.description.indexOf(searchTerm) >0; } )
                 // return processed items
-                return foundItems;
+                return result;
             });
         }
 
