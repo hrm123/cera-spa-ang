be666949acbffd96bdec866108c7209e0aa6caef
bestc0der <hrammohan@yahoo.com>
2016-11-15 18:19:20 -0500

    delta

--- module4-solution/scripts/categories.component.js
+++ module4-solution/scripts/categories.component.js
@@ -1,7 +1,7 @@
 (function(){
      var app = angular.module('MenuApp');
      app.component('menuCategories',{
-         template: "<ul ng-repeat='item in $ctrl.categoryData'><li>item</li></ul>",
+         template: "<ul ng-repeat='item in $ctrl.categoryData'><li>item.name</li></ul>",
          bindings:{
              categoryData:'<'
          },
