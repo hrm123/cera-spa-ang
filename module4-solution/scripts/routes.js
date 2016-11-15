6563fb072f74137df86edc8e4e9501b95ccbd472
bestc0der <hrammohan@yahoo.com>
2016-11-15 11:26:34 -0500

    delta

--- module4-solution/scripts/routes.js
+++ module4-solution/scripts/routes.js
@@ -1,29 +1,34 @@
 (function(){
     var app = angular.module('menuApp');
-    app.config(function($routeProvider, $locationProvider) {
-        $routeProvider
-        .when('/items/:categoryName', {
-            templateUrl: 'items.html',
-            controller: 'MenuController as ctrl',
-            resolve: {
-            // I will cause a 1 second delay
-            delay: function($q, $timeout) {
-                var delay = $q.defer();
-                $timeout(delay.resolve, 1000);
-                return delay.promise;
-            }
-            }
-        })
-        .when('/categories', {
-            templateUrl: 'categories.html',
-            controller: 'MenuController as ctrl'
-        })
-        .when('/', {
-            templateUrl: 'home.html',
-            controller: 'MenuController as ctrl'
-        });
+    app.config(routesConfig);
 
-        // configure html5 to get links working on jsfiddle
-        $locationProvider.html5Mode(true);
-    });
+    
+    routesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function routesConfig($stateProvider, $urlRouterProvider) {
+
+    // Redirect to tab 1 if no other URL matches
+    $urlRouterProvider.otherwise('/home');
+
+    // Set up UI states
+    $stateProvider
+        .state('items', {
+        url: '/items/:categoryName',
+        templateUrl: 'items.html',
+        controller: 'MenuController as ctrl'
+        })
+
+        .state('categories', {
+        url: '/categories',
+        templateUrl: 'categories.html',
+        controller: 'MenuController as ctrl'
+        })
+
+        .state('home', {
+        url: '/home',
+        templateUrl: 'home.html',
+        controller: 'MenuController as ctrl'
+        });
+    }
+    
+
 })();
