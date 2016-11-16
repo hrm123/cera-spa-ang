e396437304561038daa33beda6bfddade70f6f70
bestc0der <hrammohan@yahoo.com>
2016-11-16 10:10:48 -0500

    delta

--- module4-solution/scripts/app.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    'use strict';
-    var app = angular.module('MenuApp',['data','ui.router']);
-    app.filter('encodeURI', function() {
-    return window.encodeURIComponent;
-    });
-    
-})();
