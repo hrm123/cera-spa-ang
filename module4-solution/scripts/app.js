717be8177a9eaf613f54edcdd8fec23856886bd3
bestc0der <hrammohan@yahoo.com>
2016-11-16 05:54:28 -0500

    delta

--- module4-solution/scripts/app.js
+++ module4-solution/scripts/app.js
@@ -1,6 +1,8 @@
 (function(){
     'use strict';
     var app = angular.module('MenuApp',['data','ui.router']);
-    
+    app.filter('encodeURI', function() {
+    return window.encodeURIComponent;
+    });
     
 })();
--- module4-solution/scripts/categories.component.js
+++ module4-solution/scripts/categories.component.js
@@ -1,7 +1,7 @@
 (function(){
      var app = angular.module('MenuApp');
      app.component('menuCategories',{
-         template: "<ul ng-repeat='item in $ctrl.categorydata.data'><li><a ui-sref='items/{{item.name}}'>{{item.name}}</a></li></ul>",
+         template: "<ul ng-repeat='item in $ctrl.categorydata.data'><li><a ui-sref='items/{{item.name | encodeURI}}'>{{item.name}}</a></li></ul>",
          bindings:{
              categorydata:'<'
          },
