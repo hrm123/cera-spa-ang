bab889dee3d0e8e6c2ce1ab241716fc662c9edeb
bestc0der <hrammohan@yahoo.com>
2016-11-15 20:01:00 -0500

    delta

--- module4-solution/scripts/menu_controller.js
+++ module4-solution/scripts/menu_controller.js
@@ -3,13 +3,25 @@
     app.controller('MenuController', menuController);
 
 
-    menuController.$inject = ["$http","MenuDataService"];
+    menuController.$inject = ["$http","MenuDataService","$scope"];
     
     function menuController($http,MenuDataService){
         var ctrl = this;
         ctrl.categories= {'data' : []};
         ctrl.items=[];
         ctrl.pfx  = 'pfx1';
+        $scope.$watch(angular.bind(this,function(){
+            return this.categories;
+        }), function(newVal){
+            debugger;
+        }); 
+
+         $scope.$watch(angular.bind(this,function(){
+            return this.pfx;
+        }), function(newVal){
+            debugger;
+        }); 
+
         MenuDataService.getAllCategories().then(function(data) {
              debugger; 
              ctrl.pfx  = 'pfx2';
