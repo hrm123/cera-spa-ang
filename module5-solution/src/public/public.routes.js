f337e84cd8cb4b52689412f03a61d51eede8ae2a
bestc0der <hrammohan@yahoo.com>
2016-11-23 11:16:22 -0500

    delta

--- module5-solution/src/public/public.routes.js
+++ module5-solution/src/public/public.routes.js
@@ -19,6 +19,10 @@ function routeConfig ($stateProvider) {
       url: '/',
       templateUrl: 'src/public/home/home.html'
     })
+    .state('public.signup', {
+      url: '/signup',
+      templateUrl: 'src/public/signup/signup.html'
+    })
     .state('public.menu', {
       url: '/menu',
       templateUrl: 'src/public/menu/menu.html',
